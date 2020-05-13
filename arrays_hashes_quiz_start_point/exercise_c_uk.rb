8c3735db1b382ddbcef600be2250e287392f511c
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-13 19:51:19 +0100

    Print total population of UK

--- arrays_hashes_quiz_start_point/exercise_c_uk.rb
+++ arrays_hashes_quiz_start_point/exercise_c_uk.rb
@@ -36,4 +36,10 @@ end
 
 # 4. Use a loop to find the total population of the UK.
 
+total_population = 0
 
+for country in united_kingdom
+  total_population += country[:population]
+end
+
+p total_population
