1eb2e491dd32d9ae1533f4f2a5c49b92b2c3fef9
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-13 18:49:21 +0100

    Adds number to Erik's lottery numbers

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -109,6 +109,9 @@ end
 p get_even_numbers_from_array2(users["Avril"][:lottery_numbers])
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
+p users["Erik"][:lottery_numbers].push(7)
+
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users hash
