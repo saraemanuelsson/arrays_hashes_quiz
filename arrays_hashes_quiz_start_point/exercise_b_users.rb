3a96465109e1d6868d37e99cbcc2ebb91747d1ea
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-13 18:20:12 +0100

    Prints Eriks smallest lottery number

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -71,6 +71,9 @@ puts users["Erik"][:lottery_numbers]
 puts users["Avril"][:pets][0][:species]
 
 # 5. Get the smallest of Erik's lottery numbers
+
+puts users["Erik"][:lottery_numbers].min
+
 # 6. Return an array of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
