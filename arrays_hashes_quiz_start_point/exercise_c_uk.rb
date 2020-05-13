463e6da152c18335b5f314102e822d128c17d39a
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-13 19:47:45 +0100

    Refactor loop to print name of each country in UK array

--- arrays_hashes_quiz_start_point/exercise_c_uk.rb
+++ arrays_hashes_quiz_start_point/exercise_c_uk.rb
@@ -30,12 +30,10 @@ united_kingdom << {
 
 # 3. Use a loop to print the names of all the countries in the UK.
 
-counter = 0
-number_of_countries = 4
-
-while (counter < number_of_countries)
-  p united_kingdom[counter][:name]
-  counter += 1
+for country in united_kingdom
+  p country[:name]
 end
 
 # 4. Use a loop to find the total population of the UK.
+
+
